f1c1a60e5fd791624a98153e3be081ca178bd23d
Chad Hamre <chadhamre@gmail.com>
2019-04-23 17:04:53 -0700

    feat: fetch all products

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { Page } from "@shopify/polaris";
 
 const GET_PRODUCTS = gql`
   query getProducts($cursor: String) {
-    products(first: 1, after: $cursor) {
+    products(first: 5, after: $cursor) {
       pageInfo {
         hasNextPage
       }
@@ -30,29 +30,31 @@ class Index extends React.Component {
       <Page>
         <Query query={GET_PRODUCTS}>
           {({ data, loading, error, fetchMore }) => {
-            <getProducts
-              onLoadMore={() => {
-                console.log("LOAD MORE");
-                fetchMore({
-                  variables: {
-                    cursor: data.products.edges[0].cursor
-                  },
-                  updateQuery: (previousResult, { fetchMoreResult }) => {
-                    console.log("FETCH MORE RESULTS", fetchMoreResult);
-                    if (fetchMoreResult.products.edges.length) {
-                      return [
-                        ...previousResult.products.edges,
-                        ...fetchMoreResults.products.edges
-                      ];
-                    }
-                    return previousResult.products.edges;
-                  }
-                });
-              }}
-            />;
             if (loading) return <div>loading products to memory...</div>;
             if (error) return <div>{error.message}</div>;
-            return <div>success!</div>;
+            if (data.products.pageInfo.hasNextPage) {
+              fetchMore({
+                variables: {
+                  cursor:
+                    data.products.edges[data.products.edges.length - 1].cursor
+                },
+                updateQuery: (previousResult, { fetchMoreResult }) => {
+                  let combinedData = {
+                    products: {
+                      pageInfo: { ...fetchMoreResult.products.pageInfo },
+                      edges: [
+                        ...previousResult.products.edges,
+                        ...fetchMoreResult.products.edges
+                      ],
+                      __typename: fetchMoreResult.products.__typename
+                    }
+                  };
+                  return combinedData;
+                }
+              });
+            }
+            console.log("HERE", data);
+            return <div>{JSON.stringify(data)}</div>;
           }}
         </Query>
       </Page>
