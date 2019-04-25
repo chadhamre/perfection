0b6e6ab9bd9613c4f90f126f886bb8829f777b18
Chad Hamre <chadhamre@gmail.com>
2019-04-24 22:43:25 -0700

    feat: update body

--- components/Apply.js
+++ components/Apply.js
@@ -1,44 +1,80 @@
+import gql from "graphql-tag";
+import { Mutation } from "react-apollo";
 import React, { Component } from "react";
 import { Layout, Page, Stack, Spinner, TextStyle } from "@shopify/polaris";
+
+const UPDATE_BODY = gql`
+  mutation productUpdate($input: ProductInput!) {
+    productUpdate(input: $input) {
+      product {
+        id
+      }
+    }
+  }
+`;
 
 export class Apply extends Component {
   state = {
     changes: [],
-    change: 1
+    change: 1,
+    done: false
   };
 
-  componentDidMount() {
+  componentWillMount() {
     let changes = this.props.changes;
     this.setState({
       changes: changes
     });
-
-    changes.forEach((change, index) => {
-      console.log(change);
-      this.setState({ change: index + 1 });
-    });
   }
 
   render() {
-    return (
-      <Page fullWidth>
-        <Layout>
-          <Layout.Section>
-            <Stack alignment="center">
-              <Stack.Item>
-                <Spinner />
-              </Stack.Item>
-              <Stack.Item>
-                <TextStyle>
-                  Updating {this.state.change} of {this.state.changes.length}{" "}
-                  Product Descriptions
-                </TextStyle>
-              </Stack.Item>
-            </Stack>
-          </Layout.Section>
-        </Layout>
-      </Page>
-    );
+    if (this.state.change === this.state.changes.length)
+      window.location.reload();
+    if (
+      this.state.changes.length > 0 &&
+      this.state.change <= this.state.changes.length
+    )
+      return (
+        <Page fullWidth>
+          <Layout>
+            <Layout.Section>
+              <Stack alignment="center">
+                <Stack.Item>
+                  <Spinner />
+                </Stack.Item>
+                <Stack.Item>
+                  <TextStyle>
+                    Updating {this.state.change} of {this.state.changes.length}{" "}
+                    Product Descriptions
+                  </TextStyle>
+                </Stack.Item>
+              </Stack>
+            </Layout.Section>
+          </Layout>
+          <Mutation
+            mutation={UPDATE_BODY}
+            variables={{
+              input: {
+                id: this.state.changes[this.state.change - 1].node.id,
+                descriptionHtml: this.state.changes[this.state.change - 1].node
+                  .descriptionHtmlAfter
+              }
+            }}
+            onCompleted={() => {
+              this.setState({ done: true, change: this.state.change + 1 });
+            }}
+          >
+            {(postMutation, { loading, error, data, called }) => {
+              if (!called || this.state.done) {
+                postMutation();
+                this.setState({ done: false });
+              }
+              return null;
+            }}
+          </Mutation>
+        </Page>
+      );
+    return null;
   }
 }
 
