1921936681a9b7bd5542a1ae8ed0ca1c47da30cf
Chad Hamre <chadhamre@gmail.com>
2019-05-29 08:42:34 -0700

    feat: add api version + remove return

--- server.js
+++ server.js
@@ -5,22 +5,29 @@ const { default: createShopifyAuth } = require("@shopify/koa-shopify-auth");
 const dotenv = require("dotenv");
 const { verifyRequest } = require("@shopify/koa-shopify-auth");
 const session = require("koa-session");
+
+dotenv.config();
 const logger = require("koa-logger");
 const { default: graphQLProxy } = require("@shopify/koa-shopify-graphql-proxy");
+const { ApiVersion } = require("@shopify/koa-shopify-graphql-proxy");
 const Router = require("koa-router");
 const {
   receiveWebhook,
   registerWebhook
 } = require("@shopify/koa-shopify-webhooks");
 const processPayment = require("./server/router");
-dotenv.config();
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY, TUNNEL_URL } = process.env;
+const {
+  SHOPIFY_API_SECRET_KEY,
+  SHOPIFY_API_KEY,
+  TUNNEL_URL,
+  API_VERSION
+} = process.env;
 
 app.prepare().then(() => {
   const server = new Koa();
@@ -76,12 +83,11 @@ app.prepare().then(() => {
     console.log("received webhook: ", ctx.state.webhook);
   });
 
-  server.use(graphQLProxy());
+  server.use(graphQLProxy({ version: ApiVersion.April19 }));
   router.get("*", verifyRequest(), async ctx => {
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
     ctx.res.statusCode = 200;
-    return;
   });
   server.use(router.allowedMethods());
   server.use(router.routes());
